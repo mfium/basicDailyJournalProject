4b9be42168bcd54f834ba85dafc7ccca0829993f
Mikey <fiumarelli.michael@gmail.com>
2023-04-06 17:37:21 -0500

    Finished basic Daily Journal

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+//Lodash
+// Load the full build.
+const _ = require('lodash');
+
+
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,12 +24,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var posts = [];
+let posts = [];
 
 
 app.get("/", function (req, res) {
-  res.render("home", { homeContentPage: homeStartingContent });
-  console.log(posts);
+  res.render("home", {
+    homeContentPage: homeStartingContent,
+    homeContentPost: posts
+
+  });
+
 })
 
 app.get("/about", function (req, res) {
@@ -34,6 +47,8 @@ app.get("/contact", function (req, res) {
 app.get("/compose", function (req, res) {
   res.render("compose");
 })
+
+
 
 
 app.post("/compose", function (req, res) {
@@ -55,6 +70,24 @@ app.post("/compose", function (req, res) {
 
 
 
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.kebabCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.kebabCase(post.title);
+
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        postPageTitle: post.title,
+        postPageContent: post.content
+
+      });
+    }
+
+  });
+
+})
 
 
 app.listen(3000, function () {
